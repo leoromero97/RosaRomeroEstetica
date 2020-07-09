8d5c10e69fa3e6ac2d58ef3d473c552d4a56341d
leoromero97 <leonardogerbacio@gmail.com>
2020-07-09 18:19:38 -0300

    Se dieron estilos a la seccion del pie de pagina

---
+++ script.js
@@ -0,0 +1,17 @@
+//A Partir de esta linea se ejecuta el script para el boton que aparece para subir
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 400) {
+    document.querySelector(".containerGoTop").classList.add("showGoTop");
+    document.querySelector(".subir").classList.remove("ocultarContenido");
+    document.querySelector(".up").classList.remove("ocultarContenido");
+    //activacion y desactivacion del color de la navbar
+    active.classList.add("active");
+    disable.classList.remove("active");
+  } else {
+    document.querySelector(".containerGoTop").classList.remove("showGoTop");
+    document.querySelector(".subir").classList.add("ocultarContenido");
+    document.querySelector(".up").classList.add("ocultarContenido");
+    active.classList.remove("active");
+    disable.classList.add("active");
+  }
+}

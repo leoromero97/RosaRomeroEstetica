9de3b0093ff943a4e6cb32bfc87272144132c902
leoromero97 <leonardogerbacio@gmail.com>
2020-07-10 00:19:28 -0300

    Se crearon los efectos de la barra de navegacion

--- script.js
+++ script.js
@@ -4,14 +4,23 @@ window.onscroll = function () {
     document.querySelector(".containerGoTop").classList.add("showGoTop");
     document.querySelector(".subir").classList.remove("ocultarContenido");
     document.querySelector(".up").classList.remove("ocultarContenido");
-    //activacion y desactivacion del color de la navbar
-    active.classList.add("active");
-    disable.classList.remove("active");
+
   } else {
     document.querySelector(".containerGoTop").classList.remove("showGoTop");
     document.querySelector(".subir").classList.add("ocultarContenido");
     document.querySelector(".up").classList.add("ocultarContenido");
-    active.classList.remove("active");
-    disable.classList.add("active");
   }
 }
+
+//Este script es para la topbar.
+const navSlide = () => {
+  let burger = document.querySelector(".burger");
+  let topbar = document.querySelector(".links");
+
+  burger.addEventListener("click", () => {
+    topbar.classList.toggle("sidebar");
+    //Animacion del menu hamburguesa, pasa a ser una X
+    burger.classList.toggle("toggle");
+  });
+};
+navSlide();
